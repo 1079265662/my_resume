a2a1220a2dd3594f74e04a4e20a246661c364779
‘1079265662’ <qq1079265662@163.com>
2022-03-02 17:43:54 +0800

    '取消压缩'

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,9 @@
-
-// 引入该插件(gzip压缩)
-const CompressionPlugin = require('compression-webpack-plugin')
 const path = require('path')
 const res = require('./src/config')
 const name = res.my_title || '个人简历' // page title
 module.exports = {
-  // Vue3导入Svg
   chainWebpack: config => {
+    // Vue3导入Svg
     const svgRule = config.module.rule('svg')
     // 清除已有的所有 loader。
     svgRule.uses.clear()
@@ -30,18 +27,7 @@ module.exports = {
   },
   configureWebpack: {
     // 配置标题名称
-    name: name,
-    plugins: [
-      new CompressionPlugin({
-        filename: '[path].br[query]',
-        algorithm: 'brotliCompress',
-        test: /\.(js|css|html|svg)$/,
-        compressionOptions: { level: 11 },
-        threshold: 10240,
-        minRatio: 0.8,
-        deleteOriginalAssets: true
-      })
-    ]
+    name: name
   },
   devServer: {
     // 自动打开
