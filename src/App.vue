79da037702f77f122ac1a01a8d1be5b37aea0273
‘1079265662’ <qq1079265662@163.com>
2022-03-17 16:54:01 +0800

    '更新加载页'

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,16 @@ import { onMounted } from 'vue'
 export default {
   setup () {
     onMounted(() => {
-      // 显示页面内容
-      document.getElementById('app').style.opacity = 1
-      // 隐藏加载页面
-      // document.getElementById('loading-mask').style.transform = 'translateY(-100vh)'
-      // document.getElementById('loading-mask').style.height = '0px'
-      document.getElementById('loading-mask').style.opacity = '0'
-      document.getElementById('loading-mask').style.pointerEvents = 'none'
+      // 页面加载后显示内容
+      setTimeout(() => {
+        // 显示页面内容
+        document.getElementById('app').style.opacity = 1
+        // 隐藏加载页面
+        // document.getElementById('loading-mask').style.transform = 'translateY(-100vh)'
+        // document.getElementById('loading-mask').style.height = '0px'
+        document.getElementById('loading-mask').style.opacity = '0'
+        document.getElementById('loading-mask').style.pointerEvents = 'none'
+      }, 300)
     })
   }
 }
