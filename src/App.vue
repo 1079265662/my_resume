6e4cb7ede392a3ee0601c6afce780c7ae06fae5f
‘1079265662’ <qq1079265662@163.com>
2022-03-20 15:11:44 +0800

    '更新登录页逻辑'

--- src/App.vue
+++ src/App.vue
@@ -6,22 +6,20 @@
 </template>
 
 <script>
-import { onMounted } from 'vue'
+// 渲染完毕后 启动滑块登录效果
+window.onload = function () {
+  console.log(123)
+  setTimeout(() => {
+    // 显示页面内容
+    document.getElementById('app').style.opacity = 1
+    // 隐藏加载页面
+    // document.getElementById('loading-mask').style.transform = 'translateY(-100vh)'
+    // document.getElementById('loading-mask').style.height = '0px'
+    document.getElementById('loading-mask').style.opacity = '0'
+    document.getElementById('loading-mask').style.pointerEvents = 'none'
+  }, 300)
+}
 export default {
-  setup () {
-    onMounted(() => {
-      // 页面加载
-      setTimeout(() => {
-        // 显示页面内容
-        document.getElementById('app').style.opacity = 1
-        // 隐藏加载页面
-        // document.getElementById('loading-mask').style.transform = 'translateY(-100vh)'
-        // document.getElementById('loading-mask').style.height = '0px'
-        document.getElementById('loading-mask').style.opacity = '0'
-        document.getElementById('loading-mask').style.pointerEvents = 'none'
-      }, 300)
-    })
-  }
 }
 </script>
 <style lang="scss">
