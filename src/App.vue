1b80eece1885539df512999102df67737224f7ea
liukaili <44806432+1079265662@users.noreply.github.com>
2022-03-20 14:36:34 +0800

    还原启动页逻辑

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,12 @@
 </template>
 
 <script>
-window.onload = function () {
+import { onMounted } from 'vue'
+export default {
+  setup () {
+    onMounted(() => {
+      // 页面加载
+      setTimeout(() => {
         // 显示页面内容
         document.getElementById('app').style.opacity = 1
         // 隐藏加载页面
@@ -14,7 +19,10 @@ window.onload = function () {
         // document.getElementById('loading-mask').style.height = '0px'
         document.getElementById('loading-mask').style.opacity = '0'
         document.getElementById('loading-mask').style.pointerEvents = 'none'
-} 
+      }, 300)
+    })
+  }
+}
 </script>
 <style lang="scss">
 </style>
